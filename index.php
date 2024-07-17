48a03677faf945800c9a7651da26b508dda76f46
marcos-aparicio <marcos.aparicio593@gmail.com>
2024-07-17 13:46:30 -0400

    updating index.php file

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
-<?php 
+<?php
 
-require("db.php");
+require (__DIR__ . '/database/db.php');
 $my_projects = $conn->query('SELECT * FROM github_projects')->fetchAll();
 
 ?>
 <!doctype html>
-<html lang="en">    
+<html lang="en">
 <head>
     <title>Marcos Aparicio's Portfolio</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
+    <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Ubuntu+Condensed&display=swap" rel="stylesheet">
@@ -18,10 +19,14 @@ $my_projects = $conn->query('SELECT * FROM github_projects')->fetchAll();
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
+    <!-- fontawesome and bootstrap -->
     <script src="https://kit.fontawesome.com/92b62ee1cc.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./style.css">
+
+    <!-- own styles -->
+    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="./assets/animations.css">
 </head>
 
 <body>
@@ -53,12 +58,13 @@ $my_projects = $conn->query('SELECT * FROM github_projects')->fetchAll();
             </svg>
         </div>
     </header>
-    
+
     <main>
         <section id = "home" class="home container d-flex flex-column justify-content-center align-items-center">
-            <article class="text text-center">
+            <article class="text text-center" style="position:relative;">
                 <h1 class="title">Marcos Aparicio</h1>
                 <h2 class="sub-title">Web Developer</h2>
+                <p id="special-text" title="New Website coming soon!">New Website coming soon!</p>
             </article>
             <figure class = "d-flex justify-content-center">
                 <img src="./images/devLogo.png" alt="Developer Logo" class ="rounded logo-image">
@@ -83,7 +89,7 @@ $my_projects = $conn->query('SELECT * FROM github_projects')->fetchAll();
                 </article>
                 <div class="languages__skills" >
                     <a data-bs-toggle="tooltip" data-bs-title="Javascript, 1st language I learned" ><i class="fa-brands fa-square-js" style="color:#f7e018"></i></a>
-                    <a data-bs-toggle="tooltip" data-bs-title="HTML, the structure for websites"><i class="fa-brands fa-html5" style="color:#ff5722"></i></a> 
+                    <a data-bs-toggle="tooltip" data-bs-title="HTML, the structure for websites"><i class="fa-brands fa-html5" style="color:#ff5722"></i></a>
                     <a data-bs-toggle="tooltip" data-bs-title="CSS, the styles for websites"><i class="fa-brands fa-css3-alt" style="color:#214ce5"></i></a>
                     <a data-bs-toggle="tooltip" data-bs-title="Intermediate usage of SQL"><i class="fa-solid fa-database" style="color:#feb300"></i></a>
                     <a data-bs-toggle="tooltip" data-bs-title="Intermediate usage of PHP"><i class="fa-brands fa-php" style="color:#5B54F2"></i></a>
@@ -98,21 +104,22 @@ $my_projects = $conn->query('SELECT * FROM github_projects')->fetchAll();
                 <div class="carousel-indicators mt-4 pt-4">
                     <!--first element will be active !-->
                     <button type="button" data-bs-target="#carouselProjects" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 0"></button>
-                <?php for($i = 1; $i < count($my_projects);$i++): ?>
-                    <button type="button" data-bs-target="#carouselProjects" data-bs-slide-to="<?=$i?>" aria-label="Slide <?=$i+1?>"></button>
+                <?php for ($i = 1; $i < count($my_projects); $i++): ?>
+                    <button type="button" data-bs-target="#carouselProjects" data-bs-slide-to="<?= $i ?>" aria-label="Slide <?= $i + 1 ?>"></button>
                 <?php endfor ?>
                 </div>
                 <div class="carousel-inner">
-                <?php 
-                    $active = 1; 
-                    foreach($my_projects as $project): ?>
-                    <div class="carousel-item <?php if(1 == $active++) print('active'); ?>">
+                <?php
+                    $active = 1;
+                    foreach ($my_projects as $project):
+                ?>
+                    <div class="carousel-item <?php if (1 == $active++) print ('active'); ?>">
                         <div class="carousel-item__project d-flex flex-column justify-content-center align-items-center text-center">
 
                             <h3 class = "text-capitalize"><?= $project['name'] ?></h3>
-                            <a href="<?= $project['repo_url']?>" class = "btn btn-primary">See the code!</a>
-                            <?php if($project['deploy_url'] != ""): ?>
-                                <a href="<?=$project['deploy_url']?>" class = "btn btn-success">See project!</a>
+                            <a href="<?= $project['repo_url'] ?>" class = "btn btn-primary">See the code!</a>
+                            <?php if ($project['deploy_url'] != ''): ?>
+                                <a href="<?= $project['deploy_url'] ?>" class = "btn btn-success">See project!</a>
                             <?php endif ?>
                             <p>Language: <?= $project['language'] ?></p>
                             <p>Description: <?= $project['description'] ?></p>
@@ -131,7 +138,7 @@ $my_projects = $conn->query('SELECT * FROM github_projects')->fetchAll();
                 </button>
             </div>
         </section>
-        
+
     </main>
     <footer id = "footer">
         <div class="wave2">
@@ -140,7 +147,8 @@ $my_projects = $conn->query('SELECT * FROM github_projects')->fetchAll();
             </svg>
         </div>
     </footer>
-    <script src="./script.js" defer></script>
+    <!-- own script -->
+    <script src="./assets/script.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
